aa6de7ffa4a42a18946832878b9f48e8bf8fd44d
Krei <criketgun@gmail.com>
2025-02-10 19:18:10 +0800

    redone snouz's fix to minimize nesting

--- control.lua
+++ control.lua
@@ -24,140 +24,138 @@ end
 
 local function update_target(entity_data)
     local combinator = entity_data.combinator
-    if combinator.valid then
-        local target_pos = combinator.position
+    if combinator.valid then return end
+    local target_pos = combinator.position
 
-        if combinator.direction == defines.direction.north then
-            target_pos.y = target_pos.y - 1
-        elseif combinator.direction == defines.direction.east then
-            target_pos.x = target_pos.x + 1
-        elseif combinator.direction == defines.direction.south then
-            target_pos.y = target_pos.y + 1
-        elseif combinator.direction == defines.direction.west then
-            target_pos.x = target_pos.x - 1
-        end
-        
-        local entities = combinator.surface.find_entities_filtered({
-            position = target_pos, 
-            type = {
-                "container", 
-                "logistic-container", 
-                "assembling-machine", 
-                "furnace", 
-                "lab",
-                "reactor",
-                "boiler",
-                "rocket-silo",
-                "space-platform-hub",
-                "cargo-landing-pad",
-                "agricultural-tower"
-            }
-        })
-        if #entities > 0 then
-            entity_data.target = entities[1]
-        else
-            entity_data.target = nil
-        end
+    if combinator.direction == defines.direction.north then
+        target_pos.y = target_pos.y - 1
+    elseif combinator.direction == defines.direction.east then
+        target_pos.x = target_pos.x + 1
+    elseif combinator.direction == defines.direction.south then
+        target_pos.y = target_pos.y + 1
+    elseif combinator.direction == defines.direction.west then
+        target_pos.x = target_pos.x - 1
+    end
+    
+    local entities = combinator.surface.find_entities_filtered({
+        position = target_pos, 
+        type = {
+            "container", 
+            "logistic-container", 
+            "assembling-machine", 
+            "furnace", 
+            "lab",
+            "reactor",
+            "boiler",
+            "rocket-silo",
+            "space-platform-hub",
+            "cargo-landing-pad",
+            "agricultural-tower"
+        }
+    })
+    if #entities > 0 then
+        entity_data.target = entities[1]
+    else
+        entity_data.target = nil
     end
 end
 
 local function update_signals(entity_data)
-    if (entity_data and entity_data.combinator and entity_data.combinator.valid) then
-        if not entity_data.target then
-            -- TODO: set entity light to red (or green when there's a target)
-            local control_behavior = entity_data.combinator.get_control_behavior()
-            if control_behavior.sections_count == 0 then control_behavior.add_section() end
-            control_behavior.get_section(1).filters = {}
-            return
-        elseif not entity_data.target.valid then
-            entity_data.target = nil
-            return
-        end
-
-        local entity_type = entity_data.target.type
-        local inv = {}
-        if entity_type == "container" or entity_type == "logistic-container"then
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.chest))
-        elseif entity_type == "assembling-machine" then
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.assembling_machine_input))
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.assembling_machine_output))
-            if entity_data.target.burner then
-                concat_table(inv, entity_data.target.get_inventory(defines.inventory.fuel))
-            end
-        elseif entity_type == "furnace" then
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.furnace_source))
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.furnace_result))
-        elseif entity_type == "lab" then
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.lab_input))
-        elseif entity_type == "reactor" or entity_type == "boiler" then
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.fuel))
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.burnt_result))
-        elseif entity_type == "rocket-silo" then
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.rocket_silo_rocket))
-        elseif entity_type == "space-platform-hub" then
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.hub_main))
-        elseif entity_type == "cargo-landing-pad" then
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.cargo_landing_pad_main))
-        elseif entity_type == "agricultural-tower" then
-            concat_table(inv, entity_data.target.get_inventory(defines.inventory.assembling_machine_output))
-        end
-
-        -- calculate freshness
-        local signals = {}
-        if entity_data.mode == MODE_AVERAGE then
-            local counts = {}
-            for i=1, #inv do
-                local itemStack = inv[i]
-                if itemStack and itemStack.valid_for_read and itemStack.spoil_percent > 0 then
-                    local item_name = itemStack.name
-                    if not signals[item_name] then
-                        signals[item_name] = 0
-                        counts[item_name] = 0
-                    end
-                    signals[item_name] = signals[item_name] + itemStack.spoil_percent * itemStack.count
-                    counts[item_name] = counts[item_name] + itemStack.count
-                end
-            end
-            for k,v in pairs(signals) do
-                signals[k] = math.ceil(100 - v / counts[k] * 100)
-            end
-        elseif entity_data.mode == MODE_LEAST then
-            for i=1, #inv do
-                local itemStack = inv[i]
-                if itemStack and itemStack.valid_for_read and itemStack.spoil_percent > 0 then
-                    local item_name = itemStack.name
-                    if not signals[item_name] then signals[item_name] = 0 end
-                    if signals[item_name] < itemStack.spoil_percent then signals[item_name] = itemStack.spoil_percent end
-                end
-            end
-            for k,v in pairs(signals) do
-                signals[k] = math.ceil(100 - v * 100)
-            end
-        elseif entity_data.mode == MODE_MOST then
-            for i=1, #inv do
-                local itemStack = inv[i]
-                if itemStack and itemStack.valid_for_read and itemStack.spoil_percent > 0 then
-                    local item_name = itemStack.name
-                    if not signals[item_name] then signals[item_name] = 100 end
-                    if signals[item_name] > itemStack.spoil_percent then signals[item_name] = itemStack.spoil_percent end
-                end
-            end
-            for k,v in pairs(signals) do
-                signals[k] = math.ceil(100 - v * 100)
-            end
-        end
-
-        -- set signals
+    if (entity_data and entity_data.combinator and entity_data.combinator.valid) then return end
+    if not entity_data.target then
+        -- TODO: set entity light to red (or green when there's a target)
         local control_behavior = entity_data.combinator.get_control_behavior()
         if control_behavior.sections_count == 0 then control_behavior.add_section() end
-        local section = control_behavior.get_section(1)
-        section.filters = {}
-        local i = 1
-        for k,v in pairs(signals)
-        do
-            section.set_slot(i, {value = {type="item", name=k, quality="normal"}, min=v})
-            i = i + 1
+        control_behavior.get_section(1).filters = {}
+        return
+    elseif not entity_data.target.valid then
+        entity_data.target = nil
+        return
+    end
+
+    local entity_type = entity_data.target.type
+    local inv = {}
+    if entity_type == "container" or entity_type == "logistic-container"then
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.chest))
+    elseif entity_type == "assembling-machine" then
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.assembling_machine_input))
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.assembling_machine_output))
+        if entity_data.target.burner then
+            concat_table(inv, entity_data.target.get_inventory(defines.inventory.fuel))
         end
+    elseif entity_type == "furnace" then
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.furnace_source))
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.furnace_result))
+    elseif entity_type == "lab" then
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.lab_input))
+    elseif entity_type == "reactor" or entity_type == "boiler" then
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.fuel))
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.burnt_result))
+    elseif entity_type == "rocket-silo" then
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.rocket_silo_rocket))
+    elseif entity_type == "space-platform-hub" then
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.hub_main))
+    elseif entity_type == "cargo-landing-pad" then
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.cargo_landing_pad_main))
+    elseif entity_type == "agricultural-tower" then
+        concat_table(inv, entity_data.target.get_inventory(defines.inventory.assembling_machine_output))
+    end
+
+    -- calculate freshness
+    local signals = {}
+    if entity_data.mode == MODE_AVERAGE then
+        local counts = {}
+        for i=1, #inv do
+            local itemStack = inv[i]
+            if itemStack and itemStack.valid_for_read and itemStack.spoil_percent > 0 then
+                local item_name = itemStack.name
+                if not signals[item_name] then
+                    signals[item_name] = 0
+                    counts[item_name] = 0
+                end
+                signals[item_name] = signals[item_name] + itemStack.spoil_percent * itemStack.count
+                counts[item_name] = counts[item_name] + itemStack.count
+            end
+        end
+        for k,v in pairs(signals) do
+            signals[k] = math.ceil(100 - v / counts[k] * 100)
+        end
+    elseif entity_data.mode == MODE_LEAST then
+        for i=1, #inv do
+            local itemStack = inv[i]
+            if itemStack and itemStack.valid_for_read and itemStack.spoil_percent > 0 then
+                local item_name = itemStack.name
+                if not signals[item_name] then signals[item_name] = 0 end
+                if signals[item_name] < itemStack.spoil_percent then signals[item_name] = itemStack.spoil_percent end
+            end
+        end
+        for k,v in pairs(signals) do
+            signals[k] = math.ceil(100 - v * 100)
+        end
+    elseif entity_data.mode == MODE_MOST then
+        for i=1, #inv do
+            local itemStack = inv[i]
+            if itemStack and itemStack.valid_for_read and itemStack.spoil_percent > 0 then
+                local item_name = itemStack.name
+                if not signals[item_name] then signals[item_name] = 100 end
+                if signals[item_name] > itemStack.spoil_percent then signals[item_name] = itemStack.spoil_percent end
+            end
+        end
+        for k,v in pairs(signals) do
+            signals[k] = math.ceil(100 - v * 100)
+        end
+    end
+
+    -- set signals
+    local control_behavior = entity_data.combinator.get_control_behavior()
+    if control_behavior.sections_count == 0 then control_behavior.add_section() end
+    local section = control_behavior.get_section(1)
+    section.filters = {}
+    local i = 1
+    for k,v in pairs(signals)
+    do
+        section.set_slot(i, {value = {type="item", name=k, quality="normal"}, min=v})
+        i = i + 1
     end
 end
 
